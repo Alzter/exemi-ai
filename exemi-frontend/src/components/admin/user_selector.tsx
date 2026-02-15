9ac3896d73e819bdfa45716b4158d55bff6b8131
Alzter <alexsmall713@gmail.com>
2026-02-15 22:48:23 +1100

    e

--- exemi-frontend/src/components/admin/user_selector.tsx
+++ exemi-frontend/src/components/admin/user_selector.tsx
@@ -44,6 +44,7 @@ export default function UserSelector({session, setError, setUser} : any){
     useEffect(() => {
         if (nonAdminUsers.length > 0) {
             setUser(nonAdminUsers[0].username);
+            console.log(nonAdminUsers[0].username);
         }
     }, [users]);
 
