7bfa86035bb759dcc0cf14b76c9bbe54e9022358
Alzter <alexsmall713@gmail.com>
2026-02-25 01:13:08 +1100

    oh it was working the whole time lol

--- exemi-frontend/src/pages/app/index.tsx
+++ exemi-frontend/src/pages/app/index.tsx
@@ -29,7 +29,6 @@ export default function LoggedInFlow({session, setSession, setError, logOut} : a
 
     // Call the backend API to retrieve the user's units.
     async function fetchUserUnits() {
-        console.log("Fetching Units");
         setSession(
             (prev : any) => ({...prev, last_sync_date : new Date()})
         );
