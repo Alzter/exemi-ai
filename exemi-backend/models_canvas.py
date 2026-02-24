732c2862eb4b4e0a2735be36d515589329a54957
Alzter <alexsmall713@gmail.com>
2026-02-24 18:46:35 +1100

    Add some assignment fields necessary for calculating grade / group status

--- exemi-backend/models_canvas.py
+++ exemi-backend/models_canvas.py
@@ -21,6 +21,13 @@ class CanvasAssignment(BaseModel):
     due_at : datetime | None
     points_possible : float | None
     assignment_group_id : int
+    # Group category ID is only set if the assignment is group-based
+    group_category_id : int | None
+    # If this is a group assignment, boolean flag indicating whether or not
+    # students will be graded individually.
+    grade_group_students_individually : bool
+    # If true, the assignment's points don't count towards final mark
+    omit_from_final_grade : bool
 
 class CanvasAssignmentGroup(BaseModel):
     id : int
