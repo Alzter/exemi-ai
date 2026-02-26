d430115de4312cd4d015ecd1102640977410a694
Alzter <alexsmall713@gmail.com>
2026-02-26 20:42:04 +1100

    ok no idea

--- exemi-frontend/vite.config.ts
+++ exemi-frontend/vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    hmr: {
-      protocol: 'wss',       // because https page
-      host: 'www.exemi.au',
-      port: 443,
-    }
-  },
+  base: '/'
 })
