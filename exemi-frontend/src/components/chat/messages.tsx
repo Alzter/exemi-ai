2f3580e74de316ea205a1136513db0432e211877
Alzter <alexsmall713@gmail.com>
2026-03-19 14:51:29 +1100

    nevermind

--- exemi-frontend/src/components/chat/messages.tsx
+++ exemi-frontend/src/components/chat/messages.tsx
@@ -328,7 +328,6 @@ export default function ChatMessagesUI({session, isViewing, conversationID, setC
     }
 
     useEffect(() => {
-        setError(null);
         loadMessages(conversationID);
     }, [conversationID])
     // When loading the conversation,
